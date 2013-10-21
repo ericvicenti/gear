11b06911bea9b75812c0ab20b94eb91c8f0867c4
Eric Vicenti <eric@trapit.com>
2013-10-21 03:48:21 -0700

    fix up key creation script

---
+++ src/createServerKeys.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var path = require('path');
+var pem = require('pem');
+
+var args = process.argv;
+var node = args.shift();
+var script = args.shift();
+
+var localHostName = args.shift();
+var serverHost = args.shift();
+
+var keyFileName = path.join(process.cwd(), serverHost + '.key');
+var certFileName = path.join(process.cwd(), serverHost + '.crt');
+
+var rsync = require("rsyncwrapper").rsync;
+
+pem.createCertificate({
+  days: 1,
+  selfSigned: true,
+  commonName: serverHost,
+  keyBitsize: 4096
+}, function(err, keys){
+  if(err) return console.error(err);
+
+  console.log('Keys created! Saving..', keyFileName);
+  fs.writeFileSync(keyFileName, keys.serviceKey);
+  fs.writeFileSync(certFileName, keys.certificate);
+
+  rsync({
+      src: keyFileName,
+      dest: localHostName+":/root/server.key"
+  },function (error,stdout,stderr,cmd) {
+      if ( error ) {
+          // failed
+          console.log(error.message);
+      } else {
+          // success
+          console.log('Key Deployed!');
+      }
+  });
+
+  rsync({
+      src: certFileName,
+      dest: localHostName+":/root/server.crt"
+  },function (error,stdout,stderr,cmd) {
+      if ( error ) {
+          // failed
+          console.log(error.message);
+      } else {
+          // success
+          console.log('Cert Deployed!');
+      }
+  });
+
+
+});
+
