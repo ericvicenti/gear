e0ac28ec4390f068c8303f062de15dd96d307e3d
Eric Vicenti <eric@trapit.com>
2013-11-19 18:49:51 -0800

    cleanup, remove anonymous functions

--- src/client-servers.js
+++ src/client-servers.js
@@ -3,7 +3,7 @@ var _ = require('./util');
 var servers = module.exports = {};
 
 
-function createKeys() {
+servers.createKeys = function createKeys() {
   var create = _.defer();
   _.pem.createPrivateKey(2048, function(err, out) {
     if(err) return create.reject(err);
@@ -20,7 +20,7 @@ function createKeys() {
   return create.promise;  
 }
 
-function uploadFile(destHost, destUser, privateKey, destPath, fileData) {
+servers.uploadFile = function uploadFile(destHost, destUser, privateKey, destPath, fileData) {
   var upload = _.defer();
   _.prepareTempFolder().then(function(tempDir) {
     var keyFileName = _.path.join(tempDir, 'private.key');
@@ -48,15 +48,7 @@ function uploadFile(destHost, destUser, privateKey, destPath, fileData) {
   return upload.promise;  
 }
 
-// var pkey = _.fs.readFileSync('/Users/ev/.ssh/id_rsa', {encoding: 'utf8'});
-
-// uploadFile('hkr.io', 'root', '/test.txt', pkey, 'hello, server!').then(function() {
-//   console.log('done!');
-// }, function(er) {
-//   console.log(er);
-// });
-
-function leaseKeys(serverIp, userName, privateKey, hostName) {
+servers.leaseKeys = function leaseKeys(serverIp, userName, privateKey, hostName) {
   var lease = _.defer();
   _.pem.createCertificate({
     days: 365,
@@ -67,21 +59,17 @@ function leaseKeys(serverIp, userName, privateKey, hostName) {
     if(err) return lease.reject(err);
     var leasedKey = keys.serviceKey;
     var leasedCert = keys.certificate;
-
-    lease.resolve(_.Q.all([
-      uploadFile(serverIp, userName, privateKey, '/root/server.crt', leasedCert),
-      uploadFile(serverIp, userName, privateKey, '/root/server.key', leasedKey)
-    ]));
+    var doUploadFiles = _.Q.all([
+      servers.uploadFile(serverIp, userName, privateKey, '/root/server.crt', leasedCert),
+      servers.uploadFile(serverIp, userName, privateKey, '/root/server.key', leasedKey)
+    ]);
+    lease.resolve(doUploadFiles.then(function() {
+      return {
+        key: leasedKey,
+        cert: leasedCert
+      }
+    }));
 
   });
   return lease.promise;
 }
-
-
-var pkey = _.fs.readFileSync('/Users/ev/.ssh/id_rsa', {encoding: 'utf8'});
-
-leaseKeys('hkr.io', 'root', pkey, 'hkr.io').then(function() {
-  console.log('done!');
-}, function(er) {
-  console.log(er);
-});
